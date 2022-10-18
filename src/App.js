9978c3f1b11b84f295d6c7d83e126601272ad3d3
mowahidlatif <mowahidlatif@hotmail.com>
2022-10-17 19:18:55 -0500

    made changes to log in page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import LogInPage from './LogInPage';
 import NavBar from './NavBar';
 import React from 'react';
 import CssBaseline from "@material-ui/core/CssBaseline";
-import {Button, createTheme, Paper, ThemeProvider, Typography, Container, Box} from "@mui/material"
+import {Button, createTheme, Paper, ThemeProvider, Container, Box} from "@mui/material"
 import {grey, green, orange, red} from "@mui/material/colors"
 import { typography } from '@mui/system';
+import { alpha, styled } from '@mui/material/styles';
 
+
+
+const customIntro = styled(typography)({
+  display: 'flex',
+  fontsize: '50px',
+  padding: '30px',
+  margin: '30px',
+
+});
 
 function App() {
 
@@ -26,12 +36,13 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>     
-    <Container><Box bgcolor={grey[300]} width="100%" height="100vh"> 
-      <NavBar/>
-      <h1>Welcome to my Fitness App!</h1>
-      <LogInPage/>
-      </Box>
-    </Container>
+      <Container>
+        <Box bgcolor={grey[300]} width="100%" height="100vh"> 
+          <NavBar/>
+          <customIntro>Welcome to Team Fitness Tracker!</customIntro>
+          <LogInPage/>
+        </Box>
+      </Container>
     </ThemeProvider>
   );
 }
--- src/LogInPage.js
+++ src/LogInPage.js
@@ -1,46 +1,71 @@
 import React from "react";
-import Button from '@material-ui/core/Button';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { alpha, styled } from '@mui/material/styles';
 
-const LogInPage = () => {
+const CssTextField = styled(TextField)({
+  display: 'flex',
+  height: 48,
+  margin: '30px',
+  '&/MuiInput-underline': 'black',
+  '& label.Mui-focused': {
+    color: 'green',
+  },
+  '& .MuiInput-underline:after': {
+    borderBottomColor: 'green',
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: 'red',
+    },
+    '&:hover fieldset': {
+      borderColor: 'green',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: 'green',
+    },
+  },
+});
+
+const SubmitCustonBotton = styled(Button)({
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  border: 0,
+  borderRadius: 3,
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  color: 'white',
+  height: 48,
+  padding: '0 30px',
+  display: 'flex',
+});
+
+
+ export default function LogInPage(){
   return (
     <div>
       <form>
         <div>
-          <label>
-            Full Name:
-            <input type="text" name="name" />
-          </label>
-          <input type="submit" value="Submit" />
+        <CssTextField
+          label="Full Name..." id="custom-css-outlined-input"/>
         </div>
         <div>
-          <label>
-            Email:
-            <input type="email" name="name" />
-          </label>
-          <input type="submit" value="Submit" />
+        <CssTextField
+          label="Email Address..." id="custom-css-outlined-input"/>
         </div>
         <div>
-          <label>
-            Password:
-            <input type="password" name="name" />
-          </label>
-          <input type="submit" value="Submit" />
+        <CssTextField
+          label="Password..." id="custom-css-outlined-input"/>
         </div>
         <div>
-          <label>
-            Phone Number:
-            <input type="tel" name="name" />
-          </label>
-          <input type="submit" value="Submit" />
+        <CssTextField
+          label="Phone Number..." id="custom-css-outlined-input"/>
         </div>
-        <div>
-        <Button variant="contained" color="primary">
-        Log In!
-      </Button>
-        </div>
+       
+        <SubmitCustonBotton>
+        please work!
+      </SubmitCustonBotton>
+      
     </form>
     </div>
   )
 }
 
-export default LogInPage;
